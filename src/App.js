f7132966ac4acde0c34a7ec34128996288975eee
calin <calindotgabriel18@gmail.com>
2018-02-15 12:04:05 +0200

    Synth Plays eigth note on lopp

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Tone from 'tone';
+
+var synth = new Tone.Synth().toMaster();
+var loop = new Tone.Loop(function(time) {
+      synth.triggerAttackRelease("C4", "8n");
+    }, "8n").start(0);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Web Daw!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          I like the sun. I am doctor Sun.
+          Here sounds will be played.
         </p>
+        <button onClick={() => { Tone.Transport.start() }}>Play</button>
+        <button onClick={() => { Tone.Transport.stop() }}>Stop</button>
       </div>
     );
   }
+
+
 }
 
 export default App;
