f647c972c5d47295559cec59a47ca6c6e707e15b
calin <calindotgabriel18@gmail.com>
2018-03-18 13:30:10 +0200

    Add Cell w State

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Cell from './components/Cell'
+
 import Tone from 'tone';
 
 var synth = new Tone.Synth().toMaster();
@@ -13,10 +15,21 @@ var seq = new Tone.Sequence(function(time, note){
       console.log(note, ", ", time, "  ");
       synth.triggerAttackRelease(note, "4n")
     //straight quater notes
-    }, ["C4", "E4", "G4", "A4"], "4n")
+    }, ["C4", "D4", "G4", "A4"], "2n")
     .start(0);
 
+  const drums = [ 'kick', 'clap', 'snare' ]; 
+
+const sequence = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+const sequenceStyle = {
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onClickCell = this.onClickCell.bind(this);
+  }
   render() {
     return (
       <div className="App">
@@ -25,14 +38,22 @@ class App extends Component {
           <h1 className="App-title">Welcome to Web Daw!</h1>
         </header>
         <p className="App-intro">
-          Here sounds will be played.
+          Tone Test
         </p>
         <button onClick={() => { Tone.Transport.start() }}>Play</button>
         <button onClick={() => { Tone.Transport.stop() }}>Stop</button>
+        <div className="sequence">
+          <span className="label">Kick</span>
+          {sequence.map((step, i) => { 
+            return <Cell i={i} onClick={this.onClickCell}></Cell>
+          })}
+        </div>
       </div>
     );
   }
 
+  onClickCell(e) {
+  }
 
 }
 
---
+++ src/components/Cell.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+const colorMarked = {
+    backgroundColor: 'red'
+}
+const colorUnmarked = {
+    backgroundColor: 'grey'
+}
+class Cell extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {marked: false};
+        this.onClick = this.onClick.bind(this);
+    } 
+    render() { 
+        return <div onClick={this.onClick} className="cell"
+                style={this.state.marked ? colorMarked : colorUnmarked}></div>
+    }
+    onClick(e) {
+        this.props.onClick(e);
+        this.setState({marked: !this.state.marked});
+    }
+}
+ 
+export default Cell;
