4acd2feb1deb75205c527f99083ae6c3c0b73d23
CalinGabriel <calindotgabriel18@gmail.com>
2018-05-13 17:04:29 +0300

    Kick plays audio

--- src/App.js
+++ src/App.js
@@ -6,24 +6,50 @@ import Cell from './components/Cell'
 
 import Tone from 'tone';
 
-var synth = new Tone.Synth().toMaster();
-var loop = new Tone.Loop(function(time) {
-      synth.triggerAttackRelease("C4", "4n");
-    }, "2n");
 
-var seq = new Tone.Sequence(function(time, note){
-      console.log(note, ", ", time, "  ");
-      synth.triggerAttackRelease(note, "4n")
-    //straight quater notes
-  }, ["C1", "C1", "F2", "C4", "f2", "c3"], "4n")
-.start(0);
-
-const drums = [ 'kick', 'clap', 'snare' ]; 
-
+Tone.Transport.bpm.value = 122;
 const sequence = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-const sequenceStyle = {
+// var synth = new Tone.Synth().toMaster();
+// var loop = new Tone.Loop(function(time) {
+//       synth.triggerAttackRelease("C4", "4n");
+//     }, "2n");
+
+var loop = new Tone.Sequence(function(time, col){
+  // var column = matrix1.matrix[col];
+  // for (var i = 0; i < 4; i++){
+  //   if (column[i] === 1){
+  //     //slightly randomized velocities
+  //     // var vel = Math.random() * 0.5 + 0.5;
+  //     var vel = 1;
+  //     keys.get(noteNames[i]).start(time, 0, "32n", 0, vel);
+  //   }
+  // }
+  console.log("loop:", time, col);
+  if (col % 4 == 0) {
+    keys.get('Kick').start(time);
+  }
+}, sequence, "16n");
+
+var seq = new Tone.Sequence(function(time, note){
+      // console.log(note, ", ", time, "  "); 
+      // synth.triggerAttackRelease(note, "4n")
+    //straight quater notes
+  }, ["C1", "C1", "F2", "C4", "f2", "c3"], "8n")
+// .start(0);
+
+const drums = {
+   "Kick": "./res/kick.wav",
+  //  "Clap": "./res/clap.wav"
 }
+
+
+//setup a polyphonic sampler
+var keys = new Tone.Players(drums, {
+    "volume" : -10,
+    "fadeOut" : "64n",
+  }).toMaster();
+
 
 class App extends Component {
   constructor(props) {
@@ -35,24 +61,43 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Web Daw!</h1>
         </header>
-        <p className="App-intro">
-          Tone Test
-        </p>
-        <button onClick={() => { Tone.Transport.start() }}>Play</button>
-        <button onClick={() => { Tone.Transport.stop() }}>Stop</button>
-        <div className="sequence">
-          <span className="label">Kick</span>
-          {sequence.map((step, i) => { 
-            return <Cell i={i} onClick={this.onClickCell}></Cell>
-          })}
-        </div>
+        {/* <button onClick={() => { this.startPlay() }}>Play</button>
+        <button onClick={() => { this.stopPlay() }}>Stop</button> */}
+        <div className="container containerBox"> 
+          <div className="row controls">
+              <i onClick={() => { this.startPlay()}} className="material-icons">play_arrow</i>
+              <i onClick={() => { this.stopPlay()}} className="material-icons">stop</i>
+          </div>
+          <div className="row sequencer">
+            <div className="col-md-2">
+              Kick
+            </div>
+            <div className="col-md-10">
+            {sequence.map((step, i) => { 
+              return <Cell i={i} onClick={this.onClickCell}></Cell>
+            })}
+            </div> 
+          </div>
+        <div/>
+
+        </div> 
+        
       </div>
     );
   }
 
   onClickCell(e) {
+  }
+
+  startPlay() {
+    Tone.Transport.start()
+    loop.start()
+  }
+
+  stopPlay() {
+    Tone.Transport.stop() 
+    loop.stop()
   }
 
 }
