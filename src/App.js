45670928002467abba9bd017fce7633a489a9bb4
codesurfing <gc@codesicle.com>
2019-05-13 17:39:48 +0300

    try change lib

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
 
 import StepSequencer from './components/StepSequencer'
 
-import Tone from 'tone';
-
-let log = console.log;
-
-
 // 16 * 16 sixteen notes = 1 measure
 // 2 * 8 eighteen notes = 2 measures ( pace slowed ) 
-
 
 class App extends Component {
   constructor(props) {
